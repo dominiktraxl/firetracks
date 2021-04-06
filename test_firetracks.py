1ef05d77d502cb2cdf641ea7251eff1bd68c0c19
Dominik Traxl <dominik.traxl@posteo.org>
2021-04-06 19:56:17 +0200

    test all land use types.

--- test_firetracks.py
+++ test_firetracks.py
@@ -16,12 +16,38 @@ import subprocess
 # scripts to run
 cmds = [
     ['python', 'create_data_description_tables.py'],
+
     ['python', '01_create_fire_event_table.py'],
+
     ['python', '02_create_land_cover_table.py', 'LC_Type1'],
+    ['python', '02_create_land_cover_table.py', 'LC_Type2'],
+    ['python', '02_create_land_cover_table.py', 'LC_Type3'],
+    ['python', '02_create_land_cover_table.py', 'LC_Type4'],
+    ['python', '02_create_land_cover_table.py', 'LC_Type5'],
+    ['python', '02_create_land_cover_table.py', 'LC_Prop1'],
+    ['python', '02_create_land_cover_table.py', 'LC_Prop2'],
+    ['python', '02_create_land_cover_table.py', 'LC_Prop3'],
+    ['python', '02_create_land_cover_table.py', 'LC_Prop1_Assessment'],
+    ['python', '02_create_land_cover_table.py', 'LC_Prop2_Assessment'],
+    ['python', '02_create_land_cover_table.py', 'LC_Prop3_Assessment'],
+    ['python', '02_create_land_cover_table.py', 'QC'],
+    ['python', '02_create_land_cover_table.py', 'LW'],
+
     ['python', '03_connect_neighboring_fire_events.py'],
     ['python', '04_find_connected_fire_events.py'],
     ['python', '05_create_fire_component_table.py'],
+
     ['python', '06_create_component_land_cover_table.py', 'LC_Type1'],
+    ['python', '06_create_component_land_cover_table.py', 'LC_Type2'],
+    ['python', '06_create_component_land_cover_table.py', 'LC_Type3'],
+    ['python', '06_create_component_land_cover_table.py', 'LC_Type4'],
+    ['python', '06_create_component_land_cover_table.py', 'LC_Type5'],
+    ['python', '06_create_component_land_cover_table.py', 'LC_Prop1'],
+    ['python', '06_create_component_land_cover_table.py', 'LC_Prop2'],
+    ['python', '06_create_component_land_cover_table.py', 'LC_Prop3'],
+    ['python', '06_create_component_land_cover_table.py', 'QC'],
+    ['python', '06_create_component_land_cover_table.py', 'LW'],
+
     ['python', '07_create_component_polygons.py'],
     ['python', '07_create_component_polygons.py', '-s'],
 ]
